9687483fbdff4287d1234095aa8c2116f4efed46
Lachlan Pitts <github@parameme.com>
2013-08-07 15:07:38 +1000

    Allow for switching between Rubymine and Rubymine EAP

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,2 +1,3 @@
-export RUBYLIB=/Applications/RubyMine.app/rb/testing/patch/common:/Applications/RubyMine.app/rb/testing/patch/bdd #Rubymine test formatters
+export RUBYMINE_APP=/Applications/RubyMine\ EAP.app
+export RUBYLIB=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd #Rubymine test formatters
 export PATH="$PATH:/usr/local/sbin"
