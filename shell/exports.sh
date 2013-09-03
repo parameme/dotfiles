aaec1db835920a75ba94f2776ba00994eea89d7e
Lachlan Pitts <github@parameme.com>
2013-09-04 00:27:46 +1000

    Update env vars for Rubymine EAP

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,4 @@
 export RUBYMINE_APP=/Applications/RubyMine\ EAP.app
-export RUBYLIB=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd #Rubymine test formatters
+export TEAMCITY_FORMATTER_FOLDERS=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd/teamcity
+export RUBYLIB=$RUBYLIB:$TEAMCITY_FORMATTER_FOLDERS
 export PATH="$PATH:/usr/local/sbin"
