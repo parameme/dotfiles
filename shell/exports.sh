634809a1b22a8d6687925c9cc56b032f97e5571b
Lachlan Pitts <github@parameme.com>
2013-09-23 10:53:05 +1000

    Rubymine EAP updated - formatter moved

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,4 +1,4 @@
 export RUBYMINE_APP=/Applications/RubyMine\ EAP.app
-export TEAMCITY_FORMATTER_FOLDERS=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd/teamcity
+export TEAMCITY_FORMATTER_FOLDERS=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd
 export RUBYLIB=$RUBYLIB:$TEAMCITY_FORMATTER_FOLDERS
 export PATH="$PATH:/usr/local/sbin"
