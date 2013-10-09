7c604f3c6f90bcbd9a285d0c8d9d4ba951e8fb7e
Lachlan Pitts <github@parameme.com>
2013-10-09 22:07:06 +1000

    Inline twe4ked/dotfiles/shell/exports.sh

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,4 +1,18 @@
+export DOTFILES=~/.dotfiles                 # here
+export ARCHFLAGS='-arch x86_64'             # archflags
+export GREP_OPTIONS='--color=auto'          # double rainbow!
+export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
+export GH_LOGIN=parameme                    # used by git-go
+
+path_add() {
+  if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    PATH="${PATH:+"$PATH:"}$1"
+  fi
+}
+
+path_add "/usr/local/sbin"
+path_add "/usr/local/share/npm/bin"
+
 export RUBYMINE_APP=/Applications/RubyMine\ EAP.app
 export TEAMCITY_FORMATTER_FOLDERS=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd
 export RUBYLIB=$RUBYLIB:$TEAMCITY_FORMATTER_FOLDERS
-export PATH="$PATH:/usr/local/sbin"
