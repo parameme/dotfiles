24f30b40e0c392757ab6cc971c2b1fe574e5a92e
Lachlan Pitts <github@parameme.com>
2013-11-11 09:03:52 +1000

    Git diff configuration

--- shell/exports.sh
+++ shell/exports.sh
@@ -14,6 +14,7 @@ path_add "/usr/local/sbin"
 path_add "/usr/local/share/npm/bin"
 path_add "/usr/local/share/python"
 
+export PATH
 export RUBYMINE_APP=/Applications/RubyMine\ EAP.app
 export TEAMCITY_FORMATTER_FOLDERS=$RUBYMINE_APP/rb/testing/patch/common:$RUBYMINE_APP/rb/testing/patch/bdd
 export RUBYLIB=$RUBYLIB:$TEAMCITY_FORMATTER_FOLDERS
