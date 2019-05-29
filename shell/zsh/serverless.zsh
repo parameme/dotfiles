b321ccc8aad5d209334b4db615b2d01998e53deb
Lachlan Pitts <github@parameme.com>
2019-05-29 11:24:30 +1000

    Various updates

---
+++ shell/zsh/serverless.zsh
@@ -0,0 +1,7 @@
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /Users/lachlanpitts/src/envision-renderer-serverless/aws/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/lachlanpitts/src/envision-renderer-serverless/aws/node_modules/tabtab/.completions/serverless.zsh
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /Users/lachlanpitts/src/envision-renderer-serverless/aws/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/lachlanpitts/src/envision-renderer-serverless/aws/node_modules/tabtab/.completions/sls.zsh
