b80bb44cd05e42a5dee7bb00c96b0f0f82aa41fa
Lachlan Pitts <github@parameme.com>
2021-06-22 16:57:00 +1000

    Remove deprecated GREP_OPTIONS

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,7 +1,6 @@
 export DOTFILES=~/.dotfiles                 # here
 export ARCHFLAGS='-arch x86_64'             # archflags
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)" # ruby-build to link Homebrew's OpenSSL (updated)
-export GREP_OPTIONS='--color=auto'          # double rainbow!
 export PATH=$HOME/bin:/usr/local/bin:$PATH  # path settings
 export GH_LOGIN=parameme                    # used by git-go
 
@@ -17,3 +16,5 @@ export PATH=$PATH:/usr/local/share/npm/bin
 export JAVA_HOME=$(/usr/libexec/java_home)
 export RUBYMINE_PROPERTIES=~/opt/jetbrains/rubymine/idea.properties
 export PATH=$PATH:~/opt/jetbrains/toolbox/bin
+
+alias  grep='grep --color=auto'          # double rainbow!
