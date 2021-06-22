81ea2dc96c5b9f2591823bd22ee9e50011fc74d0
Lachlan Pitts <github@parameme.com>
2021-06-22 22:24:40 +1000

    Remove heroku alias and avoid showstash warnings

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,2 +1,1 @@
-alias heroku="/usr/local/heroku/bin/heroku"
-showstash() { git show --stat stash@{$1:-0} && git show --stat stash@{$1:-0}^3 2>/dev/null || echo No untracked files -; }
+showstash() { git show --stat stash@\{"$1":-0\} && git show --stat stash@\{"$1":-0S\}^3 2>/dev/null || echo No untracked files -; }
